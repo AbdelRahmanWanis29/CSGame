4b68b6c2094147fe815f228e03c7c6985f63b2b2
layla <leylaelfadali@gmail.com>
2026-03-02 15:51:30 +0200

    hi

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and i dont want u!");
+        System.out.println("Hello!");
         System.out.print("Layla's branch");
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
