8c8ed4490e5aa4f4ef2e84ff901a1c5f449e458d
Abdelrahman Wanis <abdelrahmand299@gmail.com>
2026-03-04 11:42:19 +0200

    x

--- src/game/engine/cards/Card.java
+++ src/game/engine/cards/Card.java
@@ -1,10 +1,10 @@
 package game.engine.cards;
 
-public class Card {
-    private final String name;
-    private final String description;
-    private final int rarity;
-    private final boolean lucky;
+    abstract public class Card {
+        private final String name;
+        private final String description;
+        private final int rarity;
+        private final boolean lucky;
 
     public Card(String name,String description,int rarity,boolean lucky)
     {
---
+++ src/game/engine/dataloader/DataLoader.java
@@ -0,0 +1,47 @@
+package game.engine.dataloader;
+
+import game.engine.cards.*;
+import game.engine.monsters.Monster;
+
+import javax.smartcardio.Card;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class DataLoader {
+
+    public static String CARDS_FILE_NAME = "cards.csv";
+    public static String MONSTERS_FILE_NAME = "monsters.csv";
+    public static String CELLS_FILE_NAME = "cells.csv";
+
+    public static ArrayList<game.engine.cards.Card> readCards() throws IOException{
+        ArrayList<game.engine.cards.Card> cards = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader(CARDS_FILE_NAME));
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] tokens = line.split(",");
+            String type = tokens[0];
+            game.engine.cards.Card c=null;
+            switch (type) {
+                case "SWAPPER":c = new SwapperCard(tokens[1],tokens[2],Integer.parseInt(tokens[3]));
+                break;
+                case "STARTOVER":c = new StartOverCard(tokens[1],tokens[2],Integer.parseInt(tokens[3]),Boolean.parseBoolean(tokens[4]));
+                break;
+                case "ENERGYSTEAL":c = new EnergyStealCard(tokens[1],tokens[2],Integer.parseInt(tokens[3]),Integer.parseInt(tokens[4]));
+                break;
+                case "SHIELD":c = new ShieldCard(tokens[1],tokens[2],Integer.parseInt(tokens[3]));
+                break;
+                case "CONFUSION":c = new ConfusionCard(tokens[1],tokens[2],Integer.parseInt(tokens[3]),Boolean.parseBoolean(tokens[4]),Integer.parseInt(tokens[5]));
+                break;
+                default:break;
+            }
+            cards.add(c);
+        }
+        return cards;
+    }
+
+
+}
