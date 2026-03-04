39149635df7973064d5b973aba2acdb109e7472a
maryam <maryamabdel6@gmail.com>
2026-03-04 13:25:12 +0200

    K

--- src/game/engine/cells/ConveyorBelt.java
+++ src/game/engine/cells/ConveyorBelt.java
@@ -1,7 +1,7 @@
 package game.engine.cells;
 
 public class ConveyorBelt extends TransportCell{
-    public ConveyorBelt(String name, int effect) {
+    public ConveyorBelt(String nam, int effect) {
         super(name,effect);
 
     }
