7b4f447dee357ba4bab394cb7d72d61f940cfb0e
layla <leylaelfadali@gmail.com>
2026-03-05 13:51:44 +0200

    hi

--- src/game/engine/monsters/Dasher.java
+++ src/game/engine/monsters/Dasher.java
@@ -1,17 +1,17 @@
 package game.engine.monsters;
 import game.engine.Role;
 
-public class Dasher extends Monster {
-    private int mommentumTurns;
+public abstract class Dasher extends Monster {
+    private int momentumTurns;
     public Dasher(String name, String description, Role role, int energy) {
         super(name, description, role, energy);
-        mommentumTurns=0;
+        momentumTurns=0;
     }
 
-    public int getMommentumTurns() {
-        return mommentumTurns;
+    public int getMomentumTurns() {
+        return momentumTurns;
     }
-    public void setMommentumTurns(int mommentumTurns) {
-        this.mommentumTurns = mommentumTurns;
+    public void setMomentumTurns(int momentumTurns) {
+        this.momentumTurns = momentumTurns;
     }
 }
