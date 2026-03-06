62af891b2b1cd2596f8ef4bab6b6e4f4f7e2778f
layla <leylaelfadali@gmail.com>
2026-03-07 00:35:21 +0200

    hi

--- src/game/engine/cells/DoorCell.java
+++ src/game/engine/cells/DoorCell.java
@@ -1,8 +1,9 @@
 package game.engine.cells;
 
 import game.engine.Role;
+import game.engine.interfaces.CanisterModifier;
 
-public class DoorCell extends Cell{
+public class DoorCell extends Cell implements CanisterModifier {
     private Role role;
     private int energy;
     private boolean activated;
--- src/game/engine/exceptions/InvalidCSVFormat.java
+++ src/game/engine/exceptions/InvalidCSVFormat.java
@@ -4,15 +4,16 @@ public class InvalidCSVFormat extends RuntimeException {
     private static final String MSG="Invalid input detected while reading csv file, input: \n";
     private String inputLine;
 
+    public InvalidCSVFormat(String inputLine) {
+        super(MSG);
+        this.inputLine=inputLine;
+    }
+
     public InvalidCSVFormat(String message,String inputLine) {
         super(message);
         this.inputLine=inputLine;
     }
 
-    public InvalidCSVFormat(String inputLine) {
-        super(MSG);
-        this.inputLine=inputLine;
-    }
 
     public String getInputLine() {
         return this.inputLine;
